0d8066981c2d29448d319460601df524af89855d
Harshafiroz <harsha@htsqatar.com>
2026-01-14 17:30:57 +0300

    updated code

--- whatsapp_saudi/whatsapp_saudi/doctype/whatsapp/test_whatsapp.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, ERPGulf.com and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class Testwhatsapp(FrappeTestCase):
-	pass
